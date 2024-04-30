21970c537a1c30e242c0aaaa47685df8bb0d4d81
kitadoyao <168435140+kitadoyao@users.noreply.github.com>
2024-04-30 15:53:13 +0800

    Update client.py
    重构了代码

--- client.py
+++ client.py
@@ -1,18 +1,7 @@
-#加把劲！还有两个文件！
-#好短的代码，我喜欢，不用写太多注释
-#去看client_socket.py，不要在这个文件问我client_socket.py的内容
 from client_socket import ClientSocket
-#同上，去看thread.py文件
-from thread import Thread
-class client:
+from thread_manager import ThreadManager
+class Client:
     def __init__(self):
-        self.socket=ClientSocket()#我创建啦！
-        self.thread=Thread(self.socket)#我也创建啦！
-        self.socket.start()#啦啦啦啦啦~
-        self.thread.start_thread(
-            send_thread=self.socket.send,#懂我说的意思了吧？
-            receive_thread=self.socket.receive,
-            heartbeat_thread=self.socket.heartbeat
-        )
-        if not self.thread.condition:
-            self.socket.stop()#如果你死了，那么我也死了
+        self.socket=ClientSocket()
+        self.thread=ThreadManager(self.socket)
+        self.thread.start_thread(self.socket.send,self.socket.receive,self.socket.heartbeat)
