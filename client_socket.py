b64d31465288e34ff89a967cc4d9aa01c750c94f
kitadoyao <168435140+kitadoyao@users.noreply.github.com>
2024-04-30 15:52:31 +0800

    Update client_socket.py
    重构了代码

--- client_socket.py
+++ client_socket.py
@@ -1,113 +1,152 @@
-#太长了，不想写注释怎么办？
-#啊啊啊！怎么办呢？！
-#还是写吧...
-#引入socket模块
-#我想吐槽这个名字，为什么要翻译成套接字
 import socket
-#引入time模块，处理时间相关的
 import time
-#终于！终于！
-#终于看到我写的东东了！【雾（】
-#这个是config.py的东东，不知道的赶紧去看
-from config import Config
-#这个是message.py的东东，不知道的赶紧去看
+from config import Config,Instruction,MessageType,Error
 from message import Message
-#定义ClientSocket类~
-#啊吧啊吧，好长啊...
 class ClientSocket:
-    #一个初始化，不会有人不知道吧
     def __init__(self):
-        #欸？为什么这里可以放函数？
-        #当然可以放，现在我告诉你了
         self._init_socket()
-        #取本机ip地址
         self.id=socket.gethostname()
-        #我还活着
         self.running=True
-        #服务端没死
         self.server_disconnected=False
+        self.send(MessageType.inquire,Instruction.please,self.id,Config.server_id)
     def _init_socket(self):
-        #创建一个socket对象
-        self.socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        #我连的服务端【雾（】
-        self.socket.connect((Config.serverHost,Config.usingPort))
-    def start(self):
-        #事先给服务端发送个信息
-        #我给大伙们翻译翻译
-        #“啊！亲爱的服务端酱！请让我这个客户端小妹妹加入你的聊天吧！”
-        self.send(Config.MessageType.inquire,Config.Instruction.please,self.id,Config.serverId)
+        try:
+            self.running=True
+            self.socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            self.socket.connect((Config.host,Config.port))
+        except socket.error as error:
+            print(f'Error connecting to the server:{error}')
     def stop(self):
-        #如果我死了
         self.running=False
-        #等于我死了
         self.socket.close()
-        #大伙们都能看懂，我就不说了
     def send(self,type,instruction,sender,addressee,content=''):
-        MESSAGE=Message.dumps(type,instruction,sender,addressee,content)
-        self.socket.sendall(MESSAGE)
-        self.handle_send(MESSAGE)
-        #懂的都懂
-    def receive(self):
-        MESSAGE=self.socket.recv(Config.maximumTextLimit).decode()
-        self.handle_receive(MESSAGE) if Message.is_message(MESSAGE) else None
-    #啊啊啊！！！好长啊！！！我要放弃了！！！写注释好累！！！
+        try:
+            message=Message.dump(type,instruction,sender,addressee,content)
+            self.socket.sendall(message)
+        except socket.error as error:
+            print(f'Error sending message:{error}')
+            if not self.reconnect():
+                self.stop()
     def handle_send(self,message):
-        match message['type']:
-            case Config.MessageType.inquire:
-                match message["instruction"]:
-                    #如果我说了一声“bye-bye”，那就是真的bye-bye！
-                    case Config.Instruction.bye:
-                        self.stop()
-    def handle_receive(self,message):
-        #这么简单的代码，真的有人看不懂吗？
-        if message is not None:
-            DICTIONARY=Message.loads(message)
-            if type(DICTIONARY)==dict:
-                match DICTIONARY['type']:
-                    #啊~如果你是送我的
-                    case Config.MessageType.transmit:
-                        match DICTIONARY['instruction']:
-                            #而且是文本~【只有文本啊！！！我没写怎么处理发送文件！！！】
-                            case Config.Instruction.text:
-                                print(f'A message from {DICTIONARY['sender']}\n')
-                                print('Main body:\n')
-                                print(f'{DICTIONARY['content']}\n')
-                    case Config.MessageType.detection:#活着吗？服务端大哥？
-                        match DICTIONARY['instruction']:
-                            case Config.Instruction.detect:
-                                match DICTIONARY['sender']:
-                                    case Config.serverId:
-                                        self.server_disconnected=False#没死，那就是活着
-                    case Config.MessageType.inquire:
-                        match DICTIONARY['instruction']:
-                            case Config.Instruction.bye:#你说了bye-bye了是吧？
-                                print(f'Addressee {DICTIONARY['sender']} stopped communication')
-                    case Config.MessageType.respond:
-                        match DICTIONARY['sender']:
-                            case Config.serverId:#是服务端大哥啊
-                                match DICTIONARY['instruction']:
-                                    case Config.Instruction.id:#我的名字吗？id2333，牛逼
-                                        print(f'You has been assigned an ID:{DICTIONARY['content']}')
-                                        self.sender_id=DICTIONARY['content']
-                                    case Config.Instruction.known:#服务端：“朕知道了”
-                                        print('Reconnecting to the server succeeded.')
-    def heartbeat(self):#死了吗？
-        self.send(Config.MessageType.detection,Config.Instruction.detect,self.id,Config.serverId)
+        dictionary=Message.loads(message)
+        if dictionary is not None and type(dictionary)==dict:
+            if dictionary['instruction']==Instruction.bye:
+                self.stop()
+    def receive(self):
+        try:
+            message=self.socket.recv(Config.maximum_text_limit)
+            self.handle_receive(Message.loads(message)) if Message.is_message(message) else None
+        except socket.error as error:
+            print(f'Error receiving message:{error}')
+            if not self.reconnect():
+                self.stop()
+    def error_report(self,addressee,error_type):
+        self.send(
+            MessageType.report,
+            Instruction.error,
+            self.id,
+            addressee,
+            error_type
+        )
+    def handle_receive(self,dictionary):
+        if dictionary is not None and type(dictionary)==dict:
+            match dictionary['type']:
+                case MessageType.transmit:
+                    match dictionary['instruction']:
+                        case Instruction.text:
+                            print(f'{dictionary['sender']}:{dictionary['content']}')
+                        case Instruction.file:
+                            filename=dictionary['content']['filename']
+                            file_content=dictionary['content']['file_content']
+                            with open(filename,'w') as file:
+                                file.write(file_content)
+                        case instruction if instruction in Instruction.difference({
+                            Instruction.text,
+                            Instruction.file
+                        }):
+                            self.error_report(dictionary['sender'],Error.WrongInstruction)
+                        case _:
+                            self.error_report(dictionary['sender'],Error.InstructionNotExist)
+                case MessageType.detection:
+                    match dictionary['instruction']:
+                        case Instruction.detect:
+                            match dictionary['sender']:
+                                case Config.server_id:
+                                    self.server_disconnected=False
+                                case _:
+                                    self.error_report(dictionary['sender'],Error.WrongAddressee)
+                        case instruction if instruction in Instruction.difference({
+                            Instruction.detect
+                        }):
+                            self.error_report(dictionary['sender'],Error.WrongInstruction)
+                        case _:
+                            self.error_report(dictionary['sender'],Error.InstructionNotExist)
+                case MessageType.inquire:
+                    match dictionary['instruction']:
+                        case Instruction.bye:
+                            print(f'{dictionary['sender']}:Communication stopped')
+                        case instruction if instruction in Instruction.difference({
+                            Instruction.bye
+                        }):
+                            self.error_report(dictionary['sender'],Error.WrongInstruction)
+                        case _:
+                            self.error_report(dictionary['sender'],Error.InstructionNotExist)
+                case MessageType.respond:
+                    match dictionary['instruction']:
+                        case Instruction.id:
+                            match dictionary['sender']:
+                                case Config.server_id:
+                                    self.id=dictionary["content"]
+                                    print(f'You\'ve been assigned:{dictionary["content"]}')
+                                case _:
+                                    self.error_report(dictionary['sender'],Error.WrongAddressee)
+                        case Instruction.known:
+                            match dictionary['sender']:
+                                case Config.server_id:
+                                    print('You reconnected to the server.')
+                                case _:
+                                    self.error_report(dictionary['sender'],Error.WrongAddressee)
+                        case instruction if instruction in Instruction.difference({
+                            Instruction.id,
+                            Instruction.known
+                        }):
+                            self.error_report(dictionary['sender'],Error.WrongInstruction)
+                        case _:
+                            self.error_report(dictionary['sender'],Error.InstructionNotExist)
+                case MessageType.report:
+                    match dictionary['instruction']:
+                        case Instruction.error:
+                            print(f'An error occurs:{dictionary["content"]}')
+                        case instruction if instruction in Instruction.difference({
+                            Instruction.error
+                        }):
+                            self.error_report(dictionary['sender'],Error.WrongInstruction)
+                        case _:
+                            self.error_report(dictionary['sender'],Error.InstructionNotExist)
+                case _:
+                    self.error_report(dictionary['sender'],Error.MessageTypeNotExist)
+    def heartbeat(self):
+        self.send(MessageType.detection,Instruction.detect,self.id,Config.server_id)
         self.server_disconnected=True
-        time.sleep(Config.heartbeatRate)
+        time.sleep(Config.heartbeat_rate)
         if self.server_disconnected:
             print('We temporarily lost contact with the server.')
             if not self.reconnect():
                 self.stop()
-    def reconnect(self,attempt=1):#死了，我来抢救！
-        if attempt<=Config.maximumAttemptLimit:
+    def reconnect(self):
+        attempt=1
+        while True:
+            if attempt>Config.maximum_attempt_limit:
+                break
             try:
                 self.stop()
-                self.running=True
                 self._init_socket()
-                self.send(Config.MessageType.inquire,Config.Instruction.call,self.id,Config.serverId)
+                self.send(MessageType.inquire,Instruction.call,self.id,Config.server_id)
                 return True
             except Exception as error:
+                print(f'Failed to reconnect to the server:{error}')
+                attempt+=1
+                time.sleep(Config.wait_attempt_rate)
                 print(f'Failed to reconnect to the server')
                 time.sleep(5)
                 self.reconnect(attempt+1)
