9a191c35278697d87671f4a43a31bf7567b1ca8b
kitadoyao <168435140+kitadoyao@users.noreply.github.com>
2024-05-05 23:06:47 +0800

    Update server.py

--- server.py
+++ server.py
@@ -4,4 +4,4 @@ class Server:
     def __init__(self):
         self.socket=ServerSocket()
         self.thread=ThreadManager(self.socket)
-        self.thread.start_thread(self.socket.accept,self.socket.receive)
+        self.thread.start_threads(self.socket.accept,self.socket.receive)
